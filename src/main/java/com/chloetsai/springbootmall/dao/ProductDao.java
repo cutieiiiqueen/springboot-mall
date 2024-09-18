0fd0f90755bacd9a789dbcdcc5ca918b6fec28a0
Chloe <e20041015@gmail.com>
2024-09-18 16:04:44 +0800

    商品功能 - 實作查詢商品列表

--- src/main/java/com/chloetsai/springbootmall/dao/ProductDao.java
+++ src/main/java/com/chloetsai/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.chloetsai.springbootmall.dao;
 import com.chloetsai.springbootmall.dto.ProductRequest;
 import com.chloetsai.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
